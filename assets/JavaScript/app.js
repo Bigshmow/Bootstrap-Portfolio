67e30d5f6fde3c3a914ca64033b15fccca6bdcf2
Bigshmow <dstewart88@gmail.com>
2019-08-22 23:20:30 -0700

    contact page updates: JS for button fade in/out with new text and message, reworded email and # contact

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,1 +1,11 @@
 // alert("Linked Up!");
+
+$(document).on("click",".submit", function(){
+    $(".form-control").val(" ");
+    $("#bigText").val("I appreciate your interest and will get back to you soon.")
+    $(".submit").fadeToggle(1000);
+    setTimeout(() => {
+        $(".submit").text("Thank you!");
+        $(".submit").fadeToggle(1000);
+    }, 2000);
+});
